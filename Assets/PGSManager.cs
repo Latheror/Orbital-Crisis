1595abd65994dc07abf43168b99f96b560ea19ca
Romain JEGAT <romain.jegat@insa-rennes.fr>
2019-02-22 19:41:31 +0100

    Trying to make Play Game Services work

---
+++ Assets/PGSManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+using TMPro;
+
+public class PGSManager : MonoBehaviour {
+
+    public static PGSManager instance;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public TextMeshProUGUI authenticationResultText;
+
+
+    void Start () {
+
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+        // recommended for debugging:
+        PlayGamesPlatform.DebugLogEnabled = true;
+        // Activate the Google Play Games platform
+        PlayGamesPlatform.Activate();
+    }
+
+    public void OnAuthenticateButton()
+    {
+        Authentication();
+    }
+
+    void Authentication()
+    {
+        // authenticate user:
+        Social.localUser.Authenticate((bool success) => {
+            // handle success or failure
+            AuthenticateCallback(success);
+        });
+    }
+
+    void AuthenticateCallback(bool success)
+    {
+        Debug.Log("AuthenticateCallback [" + success + "]");
+
+        authenticationResultText.text = success.ToString();
+    }
+
+
+}
