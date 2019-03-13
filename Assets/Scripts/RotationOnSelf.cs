b795325cedccf8094e57ad2ec83b375acd8e2339
Emilien <escinbts12.grude@ism-laval.net>
2019-03-13 15:52:11 +0100

    UI modifications, MS & buildings icons

---
+++ Assets/Scripts/RotationOnSelf.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationOnSelf : MonoBehaviour
+{
+    public int speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime*speed);
+    }
+}
